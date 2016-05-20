0322d2ec918b32373fddeb7fedd9321ac515a24e
Steve Reinke <reinke@webit.de>
2016-05-20 14:54:58 +0200

    [FEATURE] redmine integration, refs #13

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,9 +3,14 @@ class Api::V1::BaseController < ApplicationController
   respond_to :json
 
   before_action :authenticate
+  before_action :ensure_admin_secret, only: :test_airbrake
 
   def ping
     render json: { pong: Time.now }
+  end
+
+  def test_airbrake
+    raise Api::V1::TestAirbrakeException.new('api request for testing airbrake triggered')
   end
 
   private
@@ -22,6 +27,15 @@ class Api::V1::BaseController < ApplicationController
       authenticate_or_request_with_http_token do |token, _options|
         token == Settings.api.token
       end
+    end
+  end
+
+  def ensure_admin_secret
+    if params[:admin_secret] == Settings.api.admin_secret
+      true
+    else
+      head status: 403
+      false
     end
   end
 
