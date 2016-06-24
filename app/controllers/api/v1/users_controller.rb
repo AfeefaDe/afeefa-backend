5c39656225c20683d235c1290ea5b5a270090a38
Pete Abred <pete@abred.de>
2016-06-24 13:55:30 +0200

    [FEATURE] as user: list one's own orgas, refs #34

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,12 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action :set_user, only: [:list_orgas]
 
   def list_orgas
-    orgas = Role.where(user: @user).map(&:orga)
-    render json: serialize(orgas, is_collection: true)
+    if current_api_v1_user == @user
+      orgas = Role.where(user: @user).map(&:orga)
+      render json: serialize(orgas, is_collection: true)
+    else
+      head status: :forbidden
+    end
   end
 
   def set_user
